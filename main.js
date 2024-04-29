3dbea1264e49c42776eda9efcb6db742c37d6c0c
Jo-Jae <whwodjs11@naver.com>
2024-04-29 17:26:15 +0900

    first commit, UI Setting

---
+++ main.js
@@ -0,0 +1,12 @@
+//1. const testEl = document.getElementById("test");        자바스크립스에서 html파일로접근하는 방법,document는 html 파일을 뜻함
+// const testEl = document.querySelector("#test");
+// const test2El = document.querySelector(".test2");
+//1. testEl.innerHTML = "Hello";
+// testEl.innerHTML = "Hello";
+// test2El.innerHTML = "Javascript";
+
+let result = 0;
+
+const resultEl = document.querySelector(".result");
+
+resultEl.innerHTML = result;

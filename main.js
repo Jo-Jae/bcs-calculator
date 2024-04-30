f18eef10c3f543276919ad6e77cc3703c6fd20d2
Jo-Jae <whwodjs11@naver.com>
2024-04-30 13:49:49 +0900

    onClickNumber

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ let result = 0;
 const resultEl = document.querySelector(".result");
 
 resultEl.innerHTML = result;
+
+function onClickNumber(number) {
+  if (resultEl.innerHTML.length >= 13) return;
+
+  if (resultEl.innerHTML === "0") {
+    resultEl.innerHTML = number;
+  } else {
+    resultEl.innerHTML += number;
+  }
+
+  console.log(resultEl.innerHTML.length);
+}

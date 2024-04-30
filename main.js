0677fb7a477c65304d225e112556cb0f1498db29
Jo-Jae <whwodjs11@naver.com>
2024-04-30 17:25:22 +0900

    onClickOperator

--- main.js
+++ main.js
@@ -63,3 +63,57 @@ function onClickOperator(inputOperator) {
 
   operator = inputOperator;
 }
+
+function onClickEnter() {
+  switch (operator) {
+    case "+":
+      // console.log("현재 연산자는 +입니다.");
+      result += +resultEl.innerHTML;
+      resultEl.innerHTML = result;
+      break;
+
+    case "-":
+      // console.log("현재 연산자는 -입니다.");
+      result -= +resultEl.innerHTML;
+      resultEl.innerHTML = result;
+      break;
+
+    case "*":
+      // console.log("현재 연산자는 *입니다.");
+      result = result * resultEl.innerHTML;
+      resultEl.innerHTML = result;
+      break;
+
+    case "/":
+      // console.log("현재 연산자는 /입니다.");
+      result /= +resultEl.innerHTML; //* result = result / resultEl.innerHTML; 와 같음 위의 모든 연산자 적용 가능
+      resultEl.innerHTML = result;
+      break;
+
+    //나누기 케이스 소수점 없애는 로직
+    // case "/":
+    // result = parseInt(result / +resultEl.innerHTML, 10);
+    // resultEl.innerHTML = result;
+    // break;
+
+    default:
+      console.log("잘못된 연산자입니다.");
+      break;
+  }
+}
+
+//실수.
+//   function onClickDelete() {
+//   resultEl.innerHTML.substring(0, resultEl.innerHTML.length - 1);
+// }
+
+function onClickDelete() {
+  resultEl.innerHTML = resultEl.innerHTML.substring(
+    0,
+    resultEl.innerHTML.length - 1
+  );
+
+  if (resultEl.innerHTML.length === 0) {
+    resultEl.innerHTML = 0;
+  }
+}
